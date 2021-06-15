c6f88088e366c8e5bdb455ec5c48b3f92dbaf12b
github_pflorenze <paulaflorenciav@gmail.com>
2021-06-15 16:59:59 +0200

    first commit

---
+++ app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.application
+
+object AppConstants {
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "0979e03564c032e74d4cad255de5d4e7"
+}
